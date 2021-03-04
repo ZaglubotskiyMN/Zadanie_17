7b8cf2e5f5a7bc55df577545f7e244026e6d4d13
ZaglubotskiyMN <zaglubotskii@gmail.com>
2021-03-04 18:24:39 +0400

    final_commite

--- src/part1/lessons17/task1/Main.java
+++ src/part1/lessons17/task1/Main.java
@@ -8,39 +8,34 @@ import java.util.Set;
 
 public class Main {
 
-    public static void main(String[] args)  {
-        Set<String>stringSet = new LinkedHashSet<String>();
+    public static void main(String[] args) {
+
+        Set<String> stringSet = new LinkedHashSet<>();
         stringSet.add("foo");
         stringSet.add("buzz");
         stringSet.add("bar");
         stringSet.add("fork");
-        stringSet.add("bart");
+        stringSet.add("bort");
         stringSet.add("spoon");
         stringSet.add("!");
         stringSet.add("dude");
         System.out.println("Вывод без фильтрации:");
-        for (String entry :stringSet){
-            System.out.print(entry+" ");
+        for (String entry:stringSet){
+            System.out.println(entry+" ");
         }
-        class MyList {
-            public void removeEvenLength(){
-                Iterator<String> i = stringSet.iterator();
-                while (i.hasNext()) {
-                    String word = i.next();
-                    if (word.length() % 2 == 0) {
-                        i.remove();
-                        System.out.println(i.next());
-                    }
-                }
-            }
-
-
-        }
-
-
+        System.out.println("Вывод с фильтрацией");
+        System.out.println(removeEvenLength(stringSet));
     }
 
+    public static Set<String> removeEvenLength(Set<String>set){
+        if (!set.isEmpty()) {
+            set.removeIf(s -> (s.length() % 2) == 0);
+        }
+        return set;
     }
+}
+
+
 
 
 
